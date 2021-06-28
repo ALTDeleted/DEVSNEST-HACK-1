8c40960593ce18f42e8ba630d71730a14a4463bd
adityarawat1337x <rkrawat98766@gmail.com>
2021-06-29 00:03:20 +0530

    solved refresh issue

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 window.onload = () => {
-    const ball = document.querySelector(".ball");
-    const ship = document.querySelector(".ship");
-    const blocks = document.querySelectorAll(".block");
-    const container = document.querySelector(".container");
-    const scoreEl = document.querySelector(".score");
+  const ball = document.querySelector(".ball");
+  const ship = document.querySelector(".ship");
+  const blocks = document.querySelectorAll(".block");
+  const container = document.querySelector(".container");
+  const scoreEl = document.querySelector(".score");
 
-    let score=0;
-    let validBlocks = [];
+  let score = 0;
+  let validBlocks = [];
 
-    scoreEl.innerHTML = score;
+  scoreEl.innerHTML = score;
 
-    const ballD = {
+  const ballD = {
     x1: ball.getBoundingClientRect().x,
     y1: ball.getBoundingClientRect().y,
     x2: ball.getBoundingClientRect().width,
     y2: ball.getBoundingClientRect().height,
-    };
+  };
 
-    blocks.forEach((block) => {
+  blocks.forEach((block) => {
     validBlocks.push({
       x1: block.getBoundingClientRect().x,
       y1: block.getBoundingClientRect().y,
@@ -25,38 +25,38 @@ window.onload = () => {
       y2: block.getBoundingClientRect().height,
       element: block,
     });
-    });
+  });
 
-    validBlocks.splice(validBlocks.length - 1, 1);
+  validBlocks.splice(validBlocks.length - 1, 1);
 
-    // validBlocks.forEach(block=>{
-    //   const el = document.createElement("div");
-    //   el.style.background="black";
-    //   el.style.position = "absolute";
-    //   el.style.left = `${block.x1}px`;
-    //   el.style.top = `${block.y1}px`;
-    //   el.style.width = `${block.x2}px`;
-    //   el.style.height = `${block.y2}px`;
-    //   console.log(el);
+  // validBlocks.forEach(block=>{
+  //   const el = document.createElement("div");
+  //   el.style.background="black";
+  //   el.style.position = "absolute";
+  //   el.style.left = `${block.x1}px`;
+  //   el.style.top = `${block.y1}px`;
+  //   el.style.width = `${block.x2}px`;
+  //   el.style.height = `${block.y2}px`;
+  //   console.log(el);
 
-    //   document.body.appendChild(el);
-    // })
+  //   document.body.appendChild(el);
+  // })
 
-    console.log(validBlocks);
+  console.log(validBlocks);
 
-    const Xend = window.innerWidth;
-    const Yend = window.innerHeight;
-    let shipCordinate = null;
-    // console.log(Xend, Yend);
+  const Xend = window.innerWidth;
+  const Yend = window.innerHeight;
+  let shipCordinate = null;
+  // console.log(Xend, Yend);
 
-    let i = 800,
-    j = 800,
+  let i = Xend / 2,
+    j = ship.getBoundingClientRect().y - 20,
     a = 2,
     b = -2;
 
-    const interval = setInterval(ballMove, 1);
+  const interval = setInterval(ballMove, 1);
 
-    function ballMove() {
+  function ballMove() {
     ball.style.transform = `translate(${i}px,${j}px)`;
     ballD.x1 = i;
     ballD.y1 = j;
@@ -73,14 +73,15 @@ window.onload = () => {
     if (j >= Yend) {
       b = -b;
       clearInterval(interval);
-      setTimeout(window.location.href="index.html", 500);
+      setTimeout((window.location.href = "index.html"), 500);
     }
 
     if (
       shipCordinate &&
       ballD.x2 >= shipCordinate.x &&
       ballD.x1 <= shipCordinate.x + 100 &&
-      ballD.y2 >= shipCordinate.y
+      ballD.y2 >= shipCordinate.y &&
+      ballD.y1 <= shipCordinate.y
     ) {
       b = -b;
       j = j - 2;
@@ -110,19 +111,24 @@ window.onload = () => {
     for (let index = 0; index < validBlocks.length; index++) {
       let block = validBlocks[index];
       if (
-        ballD.x1 >= block.x1 &&
-        ballD.x1 <= block.x2 + block.x1 &&
-        ballD.y1 >= block.y1 &&
-        ballD.y1 <= block.y2 + block.y1
+        (ballD.x1 >= block.x1 &&
+          ballD.x1 <= block.x2 + block.x1 &&
+          ballD.y1 >= block.y1 &&
+          ballD.y1 <= block.y2 + block.y1) ||
+        ((ballD.x1 + ballD.x2) / 2 >= block.x1 &&
+          (ballD.x1 + ballD.x2) / 2 <= block.x2 &&
+          (ballD.y1 + ballD.y2) / 2 >= block.y1 &&
+          (ballD.y1 + ballD.y2) / 2 <= block.y2 + block.y1)
       ) {
         block.element.classList.add("trans");
         b = -b;
         validBlocks.splice(index, 1);
         console.log(validBlocks.length);
-        score+=10;
+        score += 10;
         scoreEl.innerHTML = score;
         break;
       }
+
       // if(i>=block.x1 && i<=block.x2+block.x1 && j>=block.y1 && j<=block.y2+block.y1 )
       //   {
       //     block.element.classList.add("trans")
@@ -136,11 +142,16 @@ window.onload = () => {
 
     i += a;
     j += b;
-    }
+  }
 
-    document.body.addEventListener("mousemove", function (e) {
+  document.body.addEventListener("mousemove", function (e) {
     shipCordinate = ship.getBoundingClientRect();
     ship.style.left = `${e.clientX - 50}px`;
-    });
+  });
 
-  }
+  const sound = document.querySelector(".audio");
+
+  sound.addEventListener("click", () => {
+    sound.children[0].muted = !sound.children[0].muted;
+  });
+};
